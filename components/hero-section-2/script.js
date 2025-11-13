4308f32005781f3a27afbb367c1bec2ec5ed8ce6
ahmadalawieh <ahmad.alawieh77@gmail.com>
2025-11-13 14:14:37 +0200

    new structure for hero section

--- components/hero-section-2/script.js
+++
@@ -1,8 +0,0 @@
-// Fade-in animation for hero-text
-window.addEventListener('DOMContentLoaded', () => {
-  const heroText = document.querySelector('.hero-text');
-  setTimeout(() => {
-    heroText.classList.add('visible');
-  }, 200);
-});
-  
